da7c775f09fdccccb169e2ace74e847a6c82b40c
Jared Grimes <jaredlancegrimes@gmail.com>
2017-09-14 14:17:33 -0400

    added rainbow road, implemented consistency

--- matchup_algs.js
+++ matchup_algs.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    $(".consis").prop('checked', true);
+
     var ctx = document.getElementById("chart");
     var graph = new Chart(ctx, {
         type: 'bar',
@@ -14,9 +16,21 @@ $(document).ready(function() {
                     'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
                     'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
                     'rgba(255,99,132,1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
@@ -41,14 +55,25 @@ $(document).ready(function() {
 
     for (var i = 0; i < decks.length; i++) // loops through every deck
     {
-        var num = 0;
+        var sliderRange = 100;
+        var ratios = [];
 
         for (var j = 1; j < decks[i].length; j++) // loops through each matchup of the given deck decks[i]
-            num += ratio_calc(decks[i], j);
+            ratios.push(ratio_calc(decks[i], j));
 
-        num /= (decks[i].length - 1); // the deck general win percentage is the average of the overall win percentages
+        var totalFreq = decks[i].length * (sliderRange / 2);
+        ratios.push(0.5);
 
-        var str = '<div class="cont" id="' + decks[i][0].name + '"><p>' + decks[i][0].name + '<p class="num">' + num + '</p></p>' + '<form action="#"> <p class="range-field"> <input type="range" class="slide" min="0" max="100" /> </p> </form><p class="inp">50</p></div>';
+        var num = 0;
+
+        for (var m = 0; m < ratios.length; m++)
+            num += (ratios[m] * (sliderRange / 2));
+
+        num /= totalFreq;
+
+        num = num * .75 + convert(decks[i][0].consistency) * .25;
+
+        var str = '<div class="cont" id="' + decks[i][0].name + '"><p>' + decks[i][0].name + '<p class="cons">' + decks[i][0].consistency + '</p><p class="num">' + num + '</p></p>' + '<form action="#"> <p class="range-field"> <input type="range" class="slide" min="0" max="' + sliderRange + '" /> </p> </form><p class="inp">50</p></div>';
 
         graph.data.labels.push(decks[i][0].name);
         graph.data.datasets[0].data.push(num);
@@ -61,16 +86,26 @@ $(document).ready(function() {
 
     graph.update();
 
-    $(document).on('input', '.slide', function() {
-        $(this).parent().parent().parent().children('.inp').html( $(this).val() )
-
+    $('.consis').change(function() {
+        $(this).parent().parent().parent().children('.inp').html( $(this).val() ); // the input class is now equal to the value of the slider
         graph.data.datasets[0].data = [];
+        for (var i = 0; i < decks.length; i++)
+            calc(i);
+        graph.update();
+    });
 
-        for (var i = 0; i < decks.length; i++) // loops through every deck
-        {
+    $(document).on('input', '.slide', function() {
+        $(this).parent().parent().parent().children('.inp').html( $(this).val() ); // the input class is now equal to the value of the slider
+        graph.data.datasets[0].data = [];
+        for (var i = 0; i < decks.length; i++)
+            calc(i);
+        graph.update();
+    });
+
+    function calc(i){
             var ratios = [];
             var freqs = [];
-            var totalFreq = 0
+            var totalFreq = 0;
 
             for (var j = 1; j < decks[i].length; j++) { // loops through each matchup of the given deck decks[i]
                 var freq = parseInt($('#' + decks[i][j].deck).children('.inp').text());
@@ -91,13 +126,14 @@ $(document).ready(function() {
 
             num /= totalFreq;
 
+            if ($(".consis").prop('checked')) // if the consistency switch is flipped
+                num = num * .75 + convert(decks[i][0].consistency) * .25;
+
             $('#' + decks[i][0].name).children('.num').text(num);
             graph.data.datasets[0].data.push(num);
-        }
-
-        graph.update();
-    });
+    }
 });
+
 
 function ratio_calc (obj, pos){
     if (obj[pos].ratio)
@@ -122,5 +158,15 @@ function convert (text){
     else if (text == "fave")
         return 0.75;
     else if (text == "vfave")
+        return 0.875;
+    else if (text == "trash")
+        return 0.125;
+    else if (text == "bad")
+        return 0.25;
+    else if (text == "decent")
+        return 0.50;
+    else if (text == "good")
+        return 0.75;
+    else if (text == "great")
         return 0.875;
 }
--- matchups.js
+++ matchups.js
@@ -1,15 +1,23 @@
 /*
 Matchup ratios:
-even: Even - 50%
+even -  Even - 50%
 sfave - Slightly Favorable - 62.5%
 fave - Favorable - 75%
 vfave - Very Favorable - 87.5%
+
+Consistency ratios:
+trash - 12.5%
+bad - 25%
+decent - 50%
+good - 75%
+great - 87.5%
  */
 
 var decks = [
     [
         {
-            name: "gardevoir"
+            name: "gardevoir",
+            consistency: "good"
         },
         {
             deck: "metagross",
@@ -30,12 +38,17 @@ var decks = [
         {
             deck: "espeon_garb",
             ratio: "fave"
+        },
+        {
+            deck: "rainbow_road",
+            ratio: null
         }
     ]
     ,
     [
         {
-            name: "metagross"
+            name: "metagross",
+            consistency: "decent"
         },
         {
             deck: "gardevoir",
@@ -56,12 +69,17 @@ var decks = [
         {
             deck: "espeon_garb",
             ratio: "vfave"
+        },
+        {
+            deck: "rainbow_road",
+            ratio: null
         }
         ]
         ,
     [
         {
-            name: "hooh_salazzle"
+            name: "hooh_salazzle",
+            consistency: "great"
         },
         {
             deck: "metagross",
@@ -81,17 +99,22 @@ var decks = [
         },
         {
             deck: "espeon_garb",
+            ratio: "fave"
+        },
+        {
+            deck: "rainbow_road",
             ratio: null
         }
         ]
         ,
     [
         {
-            name: "golisopod_garb"
+            name: "golisopod_garb",
+            consistency: "good"
         },
         {
             deck: "gardevoir",
-            ratio: "fave"
+            ratio: "sfave"
         },
         {
             deck: "metagross",
@@ -99,7 +122,7 @@ var decks = [
         },
         {
             deck: "hooh_salazzle",
-            ratio: null
+            ratio: null // very unfavorable
         },
         {
             deck: "greninja",
@@ -108,12 +131,17 @@ var decks = [
         {
             deck: "espeon_garb",
             ratio: "fave"
+        },
+        {
+            deck: "rainbow_road",
+            ratio: "sfave"
         }
         ]
         ,
     [
         {
-            name: "greninja"
+            name: "greninja",
+            consistency: "bad"
         },
         {
             deck: "gardevoir",
@@ -122,32 +150,6 @@ var decks = [
         {
             deck: "metagross",
             ratio: "even"
-        },
-        {
-            deck: "hooh_salazzle",
-            ratio: "fave"
-        },
-        {
-            deck: "golisopod_garb",
-            ratio: null
-        },
-        {
-            deck: "espeon_garb",
-            ratio: null
-        }
-    ]
-    ,
-    [
-        {
-            name: "espeon_garb"
-        },
-        {
-            deck: "gardevoir",
-            ratio: null
-        },
-        {
-            deck: "metagross",
-            ratio: null
         },
         {
             deck: "hooh_salazzle",
@@ -158,7 +160,73 @@ var decks = [
             ratio: null
         },
         {
+            deck: "espeon_garb",
+            ratio: null
+        },
+        {
+            deck: "rainbow_road",
+            ratio: "sfave"
+        }
+    ]
+    ,
+    [
+        {
+            name: "espeon_garb",
+            consistency: "good"
+        },
+        {
+            deck: "gardevoir",
+            ratio: null
+        },
+        {
+            deck: "metagross",
+            ratio: null
+        },
+        {
+            deck: "hooh_salazzle",
+            ratio: null
+        },
+        {
+            deck: "golisopod_garb",
+            ratio: null
+        },
+        {
             deck: "greninja",
+            ratio: "fave"
+        },
+        {
+            deck: "rainbow_road",
+            ratio: null
+        }
+    ]
+    ,
+    [
+        {
+            name: "rainbow_road",
+            consistency: "decent"
+        },
+        {
+            deck: "gardevoir",
+            ratio: "fave"
+        },
+        {
+            deck: "metagross",
+            ratio: "sfave"
+        },
+        {
+            deck: "hooh_salazzle",
+            ratio: "sfave"
+        },
+        {
+            deck: "golisopod_garb",
+            ratio: null //sfave their favor
+        },
+        {
+            deck: "greninja",
+            ratio: null //sfave their favor
+        },
+        {
+            deck: "espeon_garb",
             ratio: "fave"
         }
     ]
